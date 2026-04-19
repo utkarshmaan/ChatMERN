7516ff141b789a8116aaa99187bb936ee307622b
utkarsh maan <utkarshmaan91@gmail.com>
2026-04-19 13:03:00 +0530

    server js updated

--- server/server.js
+++ server/server.js
@@ -59,8 +59,12 @@ await connectDB();
 
 
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;   
+    server.listen(PORT,()=>console.log("Server is running on PORT :"+PORT));
+}
 
-server.listen(PORT,()=>console.log("Server is running on PORT :"+PORT));
+// export server for vercel deployment
+export default server;
 
 
